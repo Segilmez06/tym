8037ccfea3e8236694da1243f88b2624e9b08462
Sarp Eren EGILMEZ <2348.sarp.egilmez.2006@gmail.com>
2023-09-19 01:42:32 +0300

    Switched to argument parser, edited assembly info and added icons 🍉

--- src/App.cs
+++ src/App.cs
@@ -11,82 +11,70 @@ namespace TYM
         public static char EmptyBlockChar = '\u0020';
     }
 
+    public class Options
+    {
+        [Value(0, MetaName = "Path", Required = true, HelpText = "Path of image file.")]
+        public string? FilePath { get; set; }
+    }
+
     public class App
     {
+        private readonly Parser CommandLineParser = Parser.Default;
+
         public App(string[] Arguments) 
         {
-            string ExecutableName = Arguments[0];
+            CommandLineParser.ParseArguments<Options>(Arguments.Skip(1)).WithParsed(Run);
+        }
 
-            string HelpMessage = $@"
-TYM - View your images in the terminal with true color
-
-Usage: {ExecutableName} <image_path>
-
-This script requires:
-- Unicode supported font
-- VT100 terminal emulator with true color support
-
-";
-
-
-            if (Arguments.Length > 1)
+        private void Run(Options CommandLineOptions)
+        {
+            string? ImagePath = CommandLineOptions.FilePath;
+            if (Path.Exists(ImagePath))
             {
-                string ImagePath = Arguments[1];
-                if (Path.Exists(ImagePath))
+                Size TermSize = new(Console.BufferWidth, Console.BufferHeight);
+                Size TargetSize = new(TermSize.Width / 2, TermSize.Height / 2);
+
+                Image<Rgba32> Source = Image.Load<Rgba32>(ImagePath);
+                Source.Mutate(x => x.Resize(new ResizeOptions()
                 {
-                    Size TermSize = new(Console.BufferWidth, Console.BufferHeight);
-                    Size TargetSize = new(TermSize.Width / 2, 40);
+                    Size = TargetSize,
+                    Mode = ResizeMode.Max,
+                    Sampler = KnownResamplers.MitchellNetravali
 
-                    Image<Rgba32> Source = Image.Load<Rgba32>(ImagePath);
-                    Source.Mutate(x => x.Resize(new ResizeOptions()
+                }));
+
+                string Buffer = "";
+                for (int y = 0; y < Source.Height / 2; y++)
+                {
+                    string Line = "";
+                    for (int x = 0; x < Source.Width; x++)
                     {
-                        Size = TargetSize,
-                        Mode = ResizeMode.Max,
-                        Sampler = KnownResamplers.MitchellNetravali
+                        Rgba32[] PixelColors = { Source[x, (y * 2)], Source[x, (y * 2) + 1] };
 
-                    }));
-
-                    string Buffer = "";
-                    for (int y = 0; y < Source.Height / 2; y++)
-                    {
-                        string Line = "";
-                        for (int x = 0; x < Source.Width; x++)
+                        if (PixelColors.Any(x => x.A == 0))
                         {
-                            Rgba32[] PixelColors = { Source[x, (y * 2)], Source[x, (y * 2) + 1] };
-
-                            if (PixelColors.Any(x => x.A == 0))
-                            {
-                                int TransparencySide = PixelColors.All(x => x.A == 0) ? 2 : ((PixelColors[0].A == 0) ? 0 : 1);
-                                Rgba32 ColoredPixel = PixelColors[(PixelColors[0].A == 0) ? 1 : 0];
-                                Line += new List<int> { 0, 1 }.Any(x => TransparencySide == x) ? $"\x1b[38;2;{ColoredPixel.R};{ColoredPixel.G};{ColoredPixel.B}m" : "\x1b[39m";
-                                Line += "\x1b[49m";
-                                Line += PixelColors.All(x => x.A == 0) ? EmptyBlockChar : ((PixelColors[0].A == 0) ? BottomBlockChar : TopBlockChar);
-                            }
-                            else
-                            {
-                                Line += $"\x1b[38;2;{PixelColors[0].R};{PixelColors[0].G};{PixelColors[0].B}m";
-                                Line += $"\x1b[48;2;{PixelColors[1].R};{PixelColors[1].G};{PixelColors[1].B}m";
-                                Line += TopBlockChar;
-                            }
-                            Line += "\x1b[0m";
+                            int TransparencySide = PixelColors.All(x => x.A == 0) ? 2 : ((PixelColors[0].A == 0) ? 0 : 1);
+                            Rgba32 ColoredPixel = PixelColors[(PixelColors[0].A == 0) ? 1 : 0];
+                            Line += new List<int> { 0, 1 }.Any(x => TransparencySide == x) ? $"\x1b[38;2;{ColoredPixel.R};{ColoredPixel.G};{ColoredPixel.B}m" : "\x1b[39m";
+                            Line += "\x1b[49m";
+                            Line += PixelColors.All(x => x.A == 0) ? EmptyBlockChar : ((PixelColors[0].A == 0) ? BottomBlockChar : TopBlockChar);
                         }
-                        Buffer += Line;
-                        Buffer += "\n";
+                        else
+                        {
+                            Line += $"\x1b[38;2;{PixelColors[0].R};{PixelColors[0].G};{PixelColors[0].B}m";
+                            Line += $"\x1b[48;2;{PixelColors[1].R};{PixelColors[1].G};{PixelColors[1].B}m";
+                            Line += TopBlockChar;
+                        }
+                        Line += "\x1b[0m";
                     }
-                    Console.Write(Buffer);
-
-                    Environment.Exit(0);
+                    Buffer += Line;
+                    Buffer += "\n";
                 }
-                else
-                {
-                    Console.Write(HelpMessage);
-                    Environment.Exit(1);
-                }
+                Console.Write(Buffer);
             }
             else
             {
-                Console.Write(HelpMessage);
-                Environment.Exit(0);
+                Console.WriteLine($"\x1b[31mError:\x1b[37m Can't open file!");
             }
         }
     }
